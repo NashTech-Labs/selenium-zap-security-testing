787297e45a246242ef9cfacac5d0e7acca7712e0
deepansh257 <deepansh.gupta@nashtechglobal.com>
2024-05-15 14:27:51 +0530

    path change

--- src/main/java/org/frontend/Utility.java
+++ src/main/java/org/frontend/Utility.java
@@ -90,12 +90,10 @@ public class Utility {
             String title = dotenv.get("reportTitleFrontend");
             String description = dotenv.get("reportDescriptionFrontend");
             String template = "traditional-html";
-            String path = System.getProperty("user.dir");
-            System.out.println("Path : " + path);
             ApiResponse response = api.reports.generate(title, template, null,
                     description, null,
                   null, null, null,
-                    null, fileName, null, path
+                    null, fileName, null, "/zap/wrk/"
                     , null );
             System.out.println("ZAP report location: " + response.toString());
         }
